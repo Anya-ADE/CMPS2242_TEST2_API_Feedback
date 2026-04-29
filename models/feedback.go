1e48ce05842964fa25e150d01b9382ec19cea77a
anya andrews <2019120080@ub.edu.bz>
2026-04-28 23:16:39 -0600

    Added Feedback struct

--- models/feedback.go
+++ models/feedback.go
@@ -1,1 +1,9 @@
 package models
+
+type Feedback struct {
+	ID      int    `json:"id"`
+	Name    int    `json:"name"`
+	Email   string `json:"email"`
+	Subject string `json:"subject"`
+	Message string `json:"message"`
+}
