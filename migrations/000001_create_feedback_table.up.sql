06c14d568f1bff3e5a7b856d71e825a2d43a7222
anya andrews <2019120080@ub.edu.bz>
2026-04-28 23:48:27 -0600

    Update SQL Schema

--- migrations/000001_create_feedback_table.up.sql
+++ migrations/000001_create_feedback_table.up.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS feeback{
+CREATE TABLE IF NOT EXISTS feeback(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     subject VARCHAR(255) NOT NULL,
     message TEXT NOT NULL
-};
+);
